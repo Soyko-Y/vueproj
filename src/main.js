101af0feb17414e5b3fe32e3f59c9f2d94273cf5
Soyko-Y <puma10009@gmail.com>
2020-12-16 18:58:43 +0300

    feat: added custom stensiljs component from link

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { applyPolyfills, defineCustomElements } from "hello-world/loader";
 
-createApp(App)
-  .use(router)
-  .mount("#app");
+const app = createApp(App)
+  .use(router);
+
+applyPolyfills().then(() => {
+  defineCustomElements().then(() => {
+    app.mount("#app");
+  });
+});
